ec7e61bc50b69ee598927e4bb00d6d72275f1291
sjproctor <sjproctor950@gmail.com>
2021-08-10 15:16:39 -0700

    adds new and create methods and form

--- app/controllers/herb_controller.rb
+++ app/controllers/herb_controller.rb
@@ -8,4 +8,24 @@ class HerbController < ApplicationController
     @herb = Herb.find(params[:id])
   end
 
+  def new
+    @herb = Herb.new
+  end
+
+  def create
+    @herb = Herb.create(herb_params)
+    if @herb.valid?
+      redirect_to herbs_path
+    else
+      redirect_to new_herb_path
+    end
+  end
+
+  # Anything below private can only be called inside the scope of this class
+  private
+  # Strong params:
+  def herb_params
+    params.require(:herb).permit(:name, :watered)
+  end
+
 end
